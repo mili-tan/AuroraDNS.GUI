07c395c801785784a2062b7373419440e6dba7b4
Milkey Tan <819774989@qq.com>
2019-09-04 19:15:28 +0800

    UrlReg

---
+++ AuroraGUI/Tools/UrlReg.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using Microsoft.Win32;
+
+namespace AuroraGUI.Tools
+{
+    class UrlReg
+    {
+        public static void Reg(string UrlLink, string FriendlyName)
+        {
+            using (var key = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Classes\\" + UrlLink))
+            {
+                string applicationLocation = Process.GetCurrentProcess().MainModule.FileName;
+
+                key.SetValue("", "URL:" + FriendlyName);
+                key.SetValue("URL Protocol", "");
+
+                using (var defaultIcon = key.CreateSubKey("DefaultIcon"))
+                {
+                    defaultIcon.SetValue("", applicationLocation + ",1");
+                }
+
+                using (var commandKey = key.CreateSubKey(@"shell\open\command"))
+                {
+                    commandKey.SetValue("", "\"" + applicationLocation + "\" \"%1\"");
+                }
+            }
+        }
+        public static void UnReg(string UrlLink)
+        {
+            Registry.CurrentUser.DeleteSubKeyTree("SOFTWARE\\Classes\\" + UrlLink);
+        }
+    }
+}

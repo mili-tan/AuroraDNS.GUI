4dee1be4f63c459138bd193f546b6fc660f140fe
Milkey Tan <819774989@qq.com>
2020-03-11 19:30:21 +0800

    fix & img

--- AuroraGUI/Tools/DNSEncoder.cs
+++ AuroraGUI/Tools/DNSEncoder.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using ARSoft.Tools.Net.Dns;
 
 namespace AuroraGUI.Tools
@@ -9,15 +10,18 @@ namespace AuroraGUI.Tools
 
         public static byte[] Encode(DnsMessage dnsQMsg)
         {
+            dnsQMsg.IsRecursionAllowed = true;
+            dnsQMsg.IsRecursionDesired = true;
+            dnsQMsg.TransactionID = Convert.ToUInt16(new Random(DateTime.Now.Millisecond).Next(10));
             var args = new object[] {false, null};
             if (info == null)
                 foreach (var mInfo in dnsQMsg.GetType().GetMethods(BindingFlags.Instance | BindingFlags.NonPublic))
                     if (mInfo.ToString() == "Int32 Encode(Boolean, Byte[] ByRef)")
                         info = mInfo;
-            info.Invoke(dnsQMsg, args);
-            var dnsBytes = args[1] as byte[];
-            if (dnsBytes[2] == 0) dnsBytes[2] = 1;
-            return dnsBytes;
+            if (info != null) info.Invoke(dnsQMsg, args);
+            //var dnsBytes = args[1] as byte[];
+            //if (dnsBytes[2] == 0) dnsBytes[2] = 1;
+            return args[1] as byte[];
         }
     }
 }

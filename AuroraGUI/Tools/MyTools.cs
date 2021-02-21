1d335193810b7db16d8cb83971d01eff35565bb9
Milkey Tan <819774989@qq.com>
2021-02-21 19:35:02 +0800

    update

--- AuroraGUI/Tools/MyTools.cs
+++ AuroraGUI/Tools/MyTools.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
-using System.Runtime.Caching;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
